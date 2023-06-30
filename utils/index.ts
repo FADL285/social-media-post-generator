28c599d8667a2bd5638c3566078c83a1f535da47
Mohamed Fadl <Mohamed.Fadl2852@gmail.com>
2023-06-30 11:43:02 +0300

    update fetchWithTimeout util Types 🚀

--- utils/index.ts
+++ utils/index.ts
@@ -1,17 +1,23 @@
-import type { NitroFetchOptions } from "nitropack";
-export async function fetchWithTimeout<T>(
-  url: string,
-  fetchOptions: NitroFetchOptions<any, any> = {}
-): Promise<T> {
-  const controller = new AbortController();
+import type {
+  NitroFetchOptions,
+  NitroFetchRequest,
+  TypedInternalResponse,
+  ExtractedRouteMethod
+} from "nitropack"
+export async function fetchWithTimeout<
+  T = unknown,
+  R extends NitroFetchRequest = NitroFetchRequest,
+  O extends NitroFetchOptions<R> = NitroFetchOptions<R>
+>(url: R, fetchOptions: O) {
+  const controller = new AbortController()
   const id = setTimeout(() => {
-    controller.abort();
-    throw new Error("Requet timed out");
-  }, 15_000);
+    controller.abort()
+    throw new Error("Request timed out")
+  }, 15_000)
   const res = await $fetch<T>(url, {
     ...fetchOptions,
-    signal: controller.signal,
-  });
-  clearTimeout(id);
-  return res;
+    signal: controller.signal
+  })
+  clearTimeout(id)
+  return res as Promise<TypedInternalResponse<R, T, ExtractedRouteMethod<R, O>>>
 }
